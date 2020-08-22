994f8d764fc417490ee86b2292d2137087efa32f
Jan Piotrowski <piotrowski+github@gmail.com>
2020-08-22 22:41:25 +0200

    no message

---
+++ functions/image/image.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require("path")
+
+exports.handler = async (event, context) => {
+  const fileName = "logo.png"
+  var imagepath = path.resolve(process.env.LAMBDA_TASK_ROOT, 'src/functions/image', fileName)
+  console.log('imagepath: ', imagepath)
+
+  if(!fs.existsSync(imagepath)) {
+    return {
+      statusCode: 404
+    }
+  }
+
+  const content = fs.readFileSync(imagepath)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'image/png',
+    },
+    body: content.toString('base64'),
+    isBase64Encoded: true
+  };
+};
